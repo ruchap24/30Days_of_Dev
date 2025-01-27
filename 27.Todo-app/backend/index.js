5871aac83ec0a55560549a6c3004fad28a4d379c
ruchap24 <patilrucha991@gmail.com>
2025-01-27 16:17:09 +0530

    completed

--- 27.Todo-app/backend/index.js
+++ 27.Todo-app/backend/index.js
@@ -1,10 +1,12 @@
 const express= require ("express");
+const {createTodo}= require("./types");
 const app=express();
 
 app.use(express.json());
 
 app.post("/todo", function(req,res){
-
+    const createPayload=req.body;
+    const parsePayload=createTodo
 })
 
 app.get("/todos",function(req,res){
